2f33657169030a780a40e978a8d04e961d450300
Antti Harju <git@anttiharju.dev>
2024-10-07 16:26:21 +0300

    Extract Total component

--- part1/1.1/src/App.jsx
+++ part1/1.1/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -14,8 +15,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      {/* <Total ... /> */}
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
