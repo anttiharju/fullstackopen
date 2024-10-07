37295f9a583c1593f5654ccf89daf04bb4b39f68
Antti Harju <antti@harju.io>
2024-10-07 20:14:46 +0300

    Add all

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,6 +16,7 @@ const Statistics = (props) => {
   const good = props.good
   const neutral = props.neutral
   const bad = props.bad
+  const all= props.all
   const average = (good - bad) / (good + neutral + bad) || 0
   const positive = `${good / (good + neutral + bad) * 100 || 0} %`
 
@@ -31,6 +32,7 @@ const Statistics = (props) => {
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
       <StatisticLine text="average" value={average} />
       <StatisticLine text="positive" value={positive} />
     </p>
@@ -42,16 +44,23 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
+    const updatedAll = all + 1
+    setAll(updatedAll)
     const updatedGood = good + 1
     setGood(updatedGood)
   }
   const handleNeutralClick = () => {
+    const updatedAll = all + 1
+    setAll(updatedAll)
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
   }
   const handleBadClick = () => {
+    const updatedAll = all + 1
+    setAll(updatedAll)
     const updatedBad = bad + 1
     setBad(updatedBad)
   }
@@ -63,7 +72,7 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text='neutral' />
       <Button handleClick={handleBadClick} text='bad' />
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   )
 }
