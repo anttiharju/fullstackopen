84ecb6b3c69cc9ce4dba34782d9a6e53acd9d1ac
Antti Harju <git@anttiharju.dev>
2024-10-09 14:07:53 +0300

    Organise all into a course module

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,7 +1,3 @@
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
-
 const Course = ({ course }) => {
   return (
     <>
@@ -9,6 +5,44 @@ const Course = ({ course }) => {
       <Content course={course} />
       <Total course={course} />
     </>
+  )
+}
+
+const Header = ({ course: { name } }) => {
+  return (
+    <h1>
+      {name}
+    </h1>
+  )
+}
+
+const Content = ({ course: { parts } }) => {
+  return (
+    <>
+      {parts.map(part =>
+          <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Total = ({ course: { parts } }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <strong>
+      <p>
+        total of {totalExercises} exercises
+      </p>
+    </strong>
   )
 }
 
--- part2/courseinfo/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = ({ course: { name } }) => {
-  return (
-    <h1>
-      {name}
-    </h1>
-  )
-}
-
-export default Header
