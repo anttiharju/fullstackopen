46763785c1f0464a84881b6204301c595e9ac911
Antti Harju <antti@harju.io>
2024-10-10 11:05:19 +0300

    Remove console.logs

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,15 +10,12 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    console.log('effect')
     axios
       .get('http://localhost:3001/persons')
       .then(response => {
-        console.log('promise fulfilled')
         setPersons(response.data)
       })
   }, [])
-  console.log('render', persons.length, 'persons')
 
   return (
     <div>
