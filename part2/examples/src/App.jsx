dec7ef2b9dcc6f60bdf7fdbdcc0cf67b18a01ef7
Antti Harju <antti@harju.io>
2024-10-10 11:46:38 +0300

    Save Changing the Importance of Notes

--- part2/examples/src/App.jsx
+++ part2/examples/src/App.jsx
@@ -33,6 +33,16 @@ const App = () => {
       })
   }
 
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(n => n.id === id ? response.data : n))
+    })
+  }
+
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
@@ -52,7 +62,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note
+          key={note.id}
+          note={note}
+          toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
