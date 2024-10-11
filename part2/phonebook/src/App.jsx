0cc8a2906a4938ce67a850731c632376e0d2d7d7
Antti Harju <antti@harju.io>
2024-10-11 17:14:32 +0300

    Complete 2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import personService from './services/persons'
 
+import Notification from './components/Notification'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
@@ -8,6 +9,7 @@ import Persons from './components/Persons'
 const App = () => {
   const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState('')
+  const [toast, setToast] = useState(null)
 
   useEffect(() => {
     personService
@@ -21,11 +23,13 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
+      <Notification message={toast} />
+
       <Filter filter={filter} setFilter={setFilter} />
 
       <h3>add a new</h3>
 
-      <PersonForm persons={persons} setPersons={setPersons} />
+      <PersonForm persons={persons} setPersons={setPersons} setToast={setToast} />
 
       <h3>Numbers</h3>
 
