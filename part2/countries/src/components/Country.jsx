b124e02e0f76d771c9257d688de4217761fbebb3
Antti Harju <antti@harju.io>
2024-10-11 20:50:14 +0300

    Extract Weather component

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,33 +1,6 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import Weather from './Weather'
 
 const Country = ({country}) => {
-  const apiKey = import.meta.env.VITE_OPEN_WEATHER
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${apiKey}`
-  const [weather, setWeather] = useState(null)
-
-  useEffect(() => {
-    console.log('fetching weather data')
-    axios
-      .get(weatherUrl)
-      .then(response => {
-        setWeather(response.data)
-      })
-  }, [weatherUrl])
-
-  if (weather){
-    console.log('temperature:', weather.main.temp)
-    console.log('id:', weather.weather[0].id)
-    console.log('icon:', weather.weather[0].icon)
-    console.log('description:', weather.weather[0].description)
-    console.log('wind:', weather.wind.speed)
-  }
-
-  const temperature = 0
-  const icon = '02n'
-  const description = 'few clouds'
-  const wind = 0
-
   return (
     <>
       <h1>{country.name.common}</h1>
@@ -40,10 +13,7 @@ const Country = ({country}) => {
         ))}
       </ul>
       <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
-      <h2>Weather in {country.capital}</h2>
-      <div>temperature {temperature} Celsius</div>
-      <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt={`${country.capital} currently has ${description}`} />
-      <div>wind {wind} m/s</div>
+      <Weather country={country} />
     </>
   )
 }
---
+++ part2/countries/src/components/Weather.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Weather = ({country}) => {
+  const key = import.meta.env.VITE_OPEN_WEATHER
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${key}`
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    console.log('fetching weather data')
+    axios
+      .get(weatherUrl)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [weatherUrl])
+
+  if (weather){
+    console.log('temperature:', weather.main.temp)
+    console.log('id:', weather.weather[0].id)
+    console.log('icon:', weather.weather[0].icon)
+    console.log('description:', weather.weather[0].description)
+    console.log('wind:', weather.wind.speed)
+
+    const temperature = 0
+    const icon = '02n'
+    const description = 'few clouds'
+    const wind = 0
+
+    return (
+      <>
+        <h2>Weather in {country.capital}</h2>
+        <div>temperature {temperature} Celsius</div>
+        <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt={`${country.capital} currently has ${description}`} />
+        <div>wind {wind} m/s</div>
+      </>
+    )
+  }
+  return (<></>)
+}
+
+export default Weather
