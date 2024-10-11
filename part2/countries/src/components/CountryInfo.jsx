09710d1c6d9c4a0b71960bc2c616ba03ab4018cd
Antti Harju <antti@harju.io>
2024-10-11 19:13:57 +0300

    Complete 2.18

--- part2/countries/src/components/CountryInfo.jsx
+++ part2/countries/src/components/CountryInfo.jsx
@@ -5,10 +5,20 @@ const CountryInfo = ({countries, filter}) => {
   const filteredCountries = countries.filter(commonNameFilter)
 
   if (filteredCountries.length === 1) {
+    const country = filteredCountries[0]
     return (
-      <div>
-        Info about one country
-      </div>
+      <>
+        <h1>{country.name.common}</h1>
+        <div>capital {country.capital}</div>
+        <div>area {country.area}</div>
+        <strong><p>languages:</p></strong>
+        <ul>
+          {Object.values(country.languages).map(language => (
+            <li key={language}>{language}</li>
+          ))}
+        </ul>
+        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+      </>
     )
   }
   return (<CountryList countries={filteredCountries} />)
