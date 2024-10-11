585c52a892fd65891e69b70885ad49e4d031180a
Antti Harju <antti@harju.io>
2024-10-11 18:56:08 +0300

    Limit list length

--- part2/countries/src/components/CountryList.jsx
+++ part2/countries/src/components/CountryList.jsx
@@ -2,13 +2,13 @@ const CountryList = ({countries, filter}) => {
   const commonNameFilter = c => c.name.common.toLowerCase().includes(filter.toLowerCase())
   const filteredCountries = countries.filter(commonNameFilter)
 
-  // if (filteredCountries.length > 10) {
-  //   return (
-  //     <p>
-  //       Too many matches, specify another filter
-  //     </p>
-  //   )
-  // }
+  if (filteredCountries.length > 10) {
+    return (
+      <p>
+        Too many matches, specify another filter
+      </p>
+    )
+  }
 
   return (
     <>
