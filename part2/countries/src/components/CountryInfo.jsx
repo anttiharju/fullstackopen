469edb1d8f168ca43aa66519e5e9d9155e21e8d0
Antti Harju <antti@harju.io>
2024-10-11 19:04:32 +0300

    Barebones setup

--- part2/countries/src/components/CountryInfo.jsx
+++ part2/countries/src/components/CountryInfo.jsx
@@ -1,7 +1,17 @@
 import CountryList from './CountryList'
 
 const CountryInfo = ({countries, filter}) => {
-  return (<CountryList countries={countries} filter={filter} />)
+  const commonNameFilter = c => c.name.common.toLowerCase().includes(filter.toLowerCase())
+  const filteredCountries = countries.filter(commonNameFilter)
+
+  if (filteredCountries.length === 1) {
+    return (
+      <div>
+        Info about one country
+      </div>
+    )
+  }
+  return (<CountryList countries={filteredCountries} />)
 }
 
 export default CountryInfo
