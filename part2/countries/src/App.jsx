3bd35db129cafecfb0229529af633965f5946554
Antti Harju <git@anttiharju.dev>
2024-10-11 18:14:27 +0300

    Rename variables

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -3,22 +3,19 @@ import axios from 'axios'
 
 const App = () => {
   const [value, setValue] = useState('')
-  const [rates, setRates] = useState({})
-  const [currency, setCurrency] = useState(null)
+  const [info, setInfo] = useState({})
+  const [country, setCountry] = useState(null)
 
   useEffect(() => {
-    console.log('effect run, currency is now', currency)
-
-    // skip if currency is not defined
-    if (currency) {
-      console.log('fetching exchange rates...')
+    if (country) {
+      console.log('fetching country info...')
       axios
-        .get(`https://open.er-api.com/v6/latest/${currency}`)
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
         .then(response => {
-          setRates(response.data.rates)
+          setInfo(response.data)
         })
     }
-  }, [currency])
+  }, [country])
 
   const handleChange = (event) => {
     setValue(event.target.value)
@@ -26,17 +23,17 @@ const App = () => {
 
   const onSearch = (event) => {
     event.preventDefault()
-    setCurrency(value)
+    setCountry(value)
   }
 
   return (
     <div>
       <form onSubmit={onSearch}>
-        currency: <input value={value} onChange={handleChange} />
-        <button type="submit">exchange rate</button>
+        find countries <input value={value} onChange={handleChange} />
+        <button type="submit">search</button>
       </form>
       <pre>
-        {JSON.stringify(rates, null, 2)}
+        {JSON.stringify(info, null, 2)}
       </pre>
     </div>
   )
