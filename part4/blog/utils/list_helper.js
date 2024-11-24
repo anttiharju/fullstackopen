5845f14ef3f880dc6c930ed17d1a7e0300548f4b
Antti Harju <git@anttiharju.dev>
2024-11-24 15:36:53 +0200

    4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => { return total + blog.likes }, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  let favorite = blogs[0];
+
+  blogs.forEach(blog => {
+    if (blog.likes > favorite.likes) {
+      favorite = blog;
+    }
+  });
+  const answer = {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+  return answer
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
