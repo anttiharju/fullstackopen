d67b55fbd033a11dd45affc9ed7648ae51cf85f7
Antti Harju <antti@harju.io>
2024-12-06 19:09:27 +0200

    4.10

--- part4/blog/test/test_helper.js
+++ part4/blog/test/test_helper.js
@@ -1,3 +1,5 @@
+const Blog = require('../models/blog')
+
 const initialBlogs = [
   {
     title: "React patterns",
@@ -37,6 +39,11 @@ const initialBlogs = [
   }
 ]
 
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
 module.exports = {
-  initialBlogs
+  initialBlogs, blogsInDb
 }
