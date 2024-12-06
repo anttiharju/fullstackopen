25e6271cac2d86e06f43bdce6604e2153356ec0c
Antti Harju <git@anttiharju.dev>
2024-12-06 19:09:27 +0200

    4.10

--- part4/blog/test/blog_api.test.js
+++ part4/blog/test/blog_api.test.js
@@ -38,6 +38,27 @@ test('blogs have property id', async () => {
   }
 })
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: "Go Proverbs",
+    author: "Rob Pike",
+    url: "https://go-proverbs.github.io",
+    likes: 4
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(n => n.title)
+  assert(titles.includes('Go Proverbs'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
