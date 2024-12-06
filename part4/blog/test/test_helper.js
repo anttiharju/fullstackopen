f9414916a69af07ca536acc04a2d52a99df5f6c3
Antti Harju <antti@harju.io>
2024-12-06 18:36:51 +0200

    Better test data
    Little duplication is better than little dependency

--- part4/blog/test/test_helper.js
+++ part4/blog/test/test_helper.js
@@ -1,14 +1,38 @@
 const initialBlogs = [
   {
-    title: 'Title1',
-    author: 'Author1',
-    url: 'url1',
-    likes: 1
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7
   },
   {
-    title: 'Title2',
-    author: 'Author2',
-    url: 'url2',
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12
+  },
+  {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10
+  },
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0
+  },
+  {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2
   }
 ]
