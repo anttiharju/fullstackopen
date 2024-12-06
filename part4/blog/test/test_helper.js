79d9303be9cd8801eecffcbe5643bae4223823db
Antti Harju <git@anttiharju.dev>
2024-12-06 18:17:37 +0200

    4.8

---
+++ part4/blog/test/test_helper.js
@@ -0,0 +1,18 @@
+const initialBlogs = [
+  {
+    title: 'Title1',
+    author: 'Author1',
+    url: 'url1',
+    likes: 1
+  },
+  {
+    title: 'Title2',
+    author: 'Author2',
+    url: 'url2',
+    likes: 2
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
