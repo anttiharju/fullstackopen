6145f358e4258b56da05f94dc8ab976406e1aa4d
Antti Harju <git@anttiharju.dev>
2024-12-15 15:57:26 +0200

    Revert "Login endpoint"
    This reverts commit edfd5e73fa857f51c5d8e67ade71d76bafa61b83.

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,7 +5,6 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -27,7 +26,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
