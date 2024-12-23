b68f04b02423bc5ef26e2194512b8719d9b2cf6e
Antti Harju <git@anttiharju.dev>
2024-12-23 18:48:42 +0200

    5.3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,6 +5,9 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setNewTitle] = useState("")
+  const [newAuthor, setNewAuthor] = useState("")
+  const [newUrl, setNewUrl] = useState("")
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -74,21 +77,67 @@ const App = () => {
     </form>
   )
 
-  const blogList = () => (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value)
+  }
+
+  const blogForm = () => (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        title:<input
+          value={newTitle}
+          onChange={handleTitleChange}
+        /><br />
+        author:<input
+          value={newAuthor}
+          onChange={handleAuthorChange}
+        /><br />
+        url:<input
+          value={newUrl}
+          onChange={handleUrlChange}
+        /><br />
+        <button type="submit">create</button>
+      </form>
+      <div>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    </>
   )
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
+
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewAuthor('')
+        setNewTitle('')
+        setNewUrl('')
+      })
+  }
 
   return (
     user === null ?
     loginForm() :
     <div>
+      <h2>blogs</h2>
       <p>{user.name} logged in <button type="submit" onClick={() => handleLogout()}>logout</button></p>
-      {blogList()}
+      {blogForm()}
     </div>
   )
 }
