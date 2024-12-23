4be33dfae7784519f0f6c3c73f14505cc26bcb3b
Antti Harju <git@anttiharju.dev>
2024-12-23 20:49:36 +0200

    5.5

---
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+  const [newTitle, setNewTitle] = useState("")
+  const [newAuthor, setNewAuthor] = useState("")
+  const [newUrl, setNewUrl] = useState("")
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+  const handleUrlChange = (event) => {
+    setNewUrl(event.target.value)
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    createBlog({
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    })
+    setNewAuthor('')
+    setNewTitle('')
+    setNewUrl('')
+  }
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        title:<input
+          value={newTitle}
+          onChange={handleTitleChange}
+        /><br />
+        author:<input
+          value={newAuthor}
+          onChange={handleAuthorChange}
+        /><br />
+        url:<input
+          value={newUrl}
+          onChange={handleUrlChange}
+        /><br />
+        <button type="submit">create</button>
+      </form>
+    </>
+  )
+}
+
+export default BlogForm
