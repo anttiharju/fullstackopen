33e65c8166806073b6904d7a659b75cf0e8948a3
Antti Harju <antti@harju.io>
2024-12-23 22:12:42 +0200

    5.8 & 5.9 - although no need to send whole blog
    https://github.com/anttiharju/fullstackopen/blob/204963bc7f4c88e98fbb597c07ec6b7183301ac6/part4/test/blog_api.test.js#L178-L196
    
    This also doesn't introduce the issue that was supposed to be
    fixed in 5.9

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -21,4 +21,22 @@ const create = async newObject => {
   return response.data
 }
 
-export default { setToken, getAll, create }
+// Will cause issues with multiple users, due to sending new amount instead of a +1.
+// But I don't think the course is concerned with that.
+const like = async (id, likes) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  // Exercise 5.8 claims that this will replace the blog, but my test
+  // https://github.com/anttiharju/fullstackopen/blob/204963bc7f4c88e98fbb597c07ec6b7183301ac6/part4/test/blog_api.test.js#L178-L196
+  // claims otherwise. I'll trust the test.
+  const newLikes = {
+    likes: likes + 1
+  }
+
+  const response = await axios.put(`api/blogs/${id}`, newLikes, config)
+  return response.data
+}
+
+export default { setToken, getAll, create, like }
