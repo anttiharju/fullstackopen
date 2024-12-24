36d9dc5f7e4dd88ee660abae48635af9cee789ab
Antti Harju <git@anttiharju.dev>
2024-12-24 14:31:54 +0200

    5.17

--- part5/e2e-tests/tests/bloglist.spec.js
+++ part5/e2e-tests/tests/bloglist.spec.js
@@ -2,10 +2,14 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
   beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
-    // ...
+    const locator = page.getByText('log in to application')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+    await expect(page.getByText('login')).toBeVisible()
   })
 })
