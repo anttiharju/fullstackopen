55b6e83cbbae4a15c74bbf1d57c0a3e753f2c4b0
Antti Harju <git@anttiharju.dev>
2024-12-24 13:34:13 +0200

    Importing sample code also doesn't fix it

--- part5/examples/e2e-tests/tests/note.spec.js
+++ part5/examples/e2e-tests/tests/note.spec.js
@@ -2,7 +2,7 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http:localhost:3001/api/testing/reset')
     await request.post('http://localhost:3001/api/users', {
       data: {
         name: 'Matti Luukkainen',
@@ -15,19 +15,18 @@ describe('Note app', () => {
   })
 
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5173')
-
     const locator = await page.getByText('Notes')
     await expect(locator).toBeVisible()
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
   })
 
-  test('user can log in', async ({ page }) => {
+  test('login form can be opened', async ({ page }) => {
     await page.getByRole('button', { name: 'log in' }).click()
     await page.getByTestId('username').fill('mluukkai')
     await page.getByTestId('password').fill('salainen')
     await page.getByRole('button', { name: 'login' }).click()
-    //await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible() // not implemented, inconsistency?
+
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
   })
 
   describe('when logged in', () => {
@@ -42,7 +41,7 @@ describe('Note app', () => {
       await page.getByRole('button', { name: 'new note' }).click()
       await page.getByRole('textbox').fill('a note created by playwright')
       await page.getByRole('button', { name: 'save' }).click()
-      await expect(page.getByText('a note created by playwright')).toBeVisible()
+      await expect(await page.getByText('a note created by playwright')).toBeVisible()
     })
 
     describe('and a note exists', () => {
@@ -54,7 +53,7 @@ describe('Note app', () => {
 
       test('importance can be changed', async ({ page }) => {
         await page.getByRole('button', { name: 'make not important' }).click()
-        await expect(page.getByText('make important')).toBeVisible()
+        await expect(await page.getByText('make important')).toBeVisible()
       })
     })
   })
