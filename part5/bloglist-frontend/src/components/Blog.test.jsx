eba0b3989f19f82be4f3bd652d1a945a30abd191
Antti Harju <git@anttiharju.dev>
2024-12-24 11:38:44 +0200

    5.15

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -51,4 +51,46 @@ describe('<Blog />', () => {
       expect(div).not.toHaveTextContent(`likes ${blog.likes}`)
     })
   })
+
+  describe('calls', () => {
+    test('event handler likeBlogs twice when \'like\' has been clicked twice', async () => {
+      const blog = {
+        title: 'Kubernetes The Hard Way',
+        author: 'Kelsey Hightower',
+        url: 'https://github.com/kelseyhightower/kubernetes-the-hard-way',
+        likes: 240,
+        user: {
+          name: 'Kelsey Hightower',
+          username: 'kelseyhightower'
+        }
+      }
+
+      // Silence PropType warnings
+      const dummyLoggedInUserUsername = 'quii' // misleading to reuse blog.user.username
+      const dummyRemoveBlog = () => {}
+
+      // Mocks
+      const mockLikeBlog = vi.fn()
+
+      render(
+        <Blog
+          blog={blog}
+          username={dummyLoggedInUserUsername}
+          likeBlog={mockLikeBlog}
+          removeBlog={dummyRemoveBlog}
+        />
+      )
+
+      const user = userEvent.setup()
+
+      const viewButton = screen.getByText('view')
+      await user.click(viewButton)
+
+      const likeButton = screen.getByText('like')
+      await user.click(likeButton)
+      await user.click(likeButton)
+
+      expect(mockLikeBlog.mock.calls).toHaveLength(2)
+    })
+  })
 })
