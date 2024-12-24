bb33972465c29816b1680671fe4d9643d54eee9e
Antti Harju <antti@harju.io>
2024-12-24 11:09:38 +0200

    Prep for next exercise
    Little duplication is better than little dependency

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -3,50 +3,52 @@ import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
 
 describe('<Blog />', () => {
-  let container
+  describe('renders', () => {
+    let container
 
-  const blog = {
-    title: 'Learn Go with Tests',
-    author: 'Chris James',
-    url: 'https://quii.gitbook.io/learn-go-with-tests',
-    likes: 352,
-    user: {
-      name: 'Chris James',
-      username: 'quii'
+    const blog = {
+      title: 'Learn Go with Tests',
+      author: 'Chris James',
+      url: 'https://quii.gitbook.io/learn-go-with-tests',
+      likes: 352,
+      user: {
+        name: 'Chris James',
+        username: 'quii'
+      }
     }
-  }
 
-  // Silence PropType warnings
-  const dummyUpdateBlog = () => {}
-  const dummyLoggedInUserUsername = 'kelseyhightower' // misleading to reuse blog.user.username
-  const dummyRemoveBlog = () => {}
+    // Silence PropType warnings
+    const dummyUpdateBlog = () => {}
+    const dummyLoggedInUserUsername = 'kelseyhightower' // misleading to reuse blog.user.username
+    const dummyRemoveBlog = () => {}
 
-  beforeEach(() => {
-    container = render(
-      <Blog
-        blog={blog}
-        updateBlog={dummyUpdateBlog}
-        username={dummyLoggedInUserUsername}
-        removeBlog={dummyRemoveBlog}
-      />
-    ).container
-  })
+    beforeEach(() => {
+      container = render(
+        <Blog
+          blog={blog}
+          updateBlog={dummyUpdateBlog}
+          username={dummyLoggedInUserUsername}
+          removeBlog={dummyRemoveBlog}
+        />
+      ).container
+    })
 
-  test('by default renders title and author but not url or likes', () => {
-    const div = container.querySelector('.blog')
-    expect(div).toHaveTextContent(blog.title)
-    expect(div).toHaveTextContent(blog.author)
-    expect(div).not.toHaveTextContent(blog.url)
-    expect(div).not.toHaveTextContent(`likes ${blog.likes}`)
-  })
+    test('title and author but not url or likes by default', () => {
+      const div = container.querySelector('.blog')
+      expect(div).toHaveTextContent(blog.title)
+      expect(div).toHaveTextContent(blog.author)
+      expect(div).not.toHaveTextContent(blog.url)
+      expect(div).not.toHaveTextContent(`likes ${blog.likes}`)
+    })
 
-  test('clicking view renders url and likes', async () => {
-    const user = userEvent.setup()
-    const button = screen.getByText('view')
-    await user.click(button)
+    test('url and likes after clicking \'view\'', async () => {
+      const user = userEvent.setup()
+      const button = screen.getByText('view')
+      await user.click(button)
 
-    const div = container.querySelector('.blog')
-    expect(div).not.toHaveTextContent(blog.url)
-    expect(div).not.toHaveTextContent(`likes ${blog.likes}`)
+      const div = container.querySelector('.blog')
+      expect(div).not.toHaveTextContent(blog.url)
+      expect(div).not.toHaveTextContent(`likes ${blog.likes}`)
+    })
   })
 })
