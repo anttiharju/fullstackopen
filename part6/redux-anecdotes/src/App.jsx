48a5a4efdd1ffb865d54eaa8b908d4920fd0e867
Antti Harju <antti@harju.io>
2024-12-24 20:54:23 +0200

    6.6 - 6.8, already did them

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,12 +1,12 @@
-import Anecdotes from './components/Anecdotes'
-import NewAnecdote from './components/NewAnecdote'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-
   return (
     <div>
-      <Anecdotes />
-      <NewAnecdote />
+      <h2>Anecdotes</h2>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
