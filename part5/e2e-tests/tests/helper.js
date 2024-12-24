b1dc7130e68e98ddc09c8f1a666322b4f933a4c0
Antti Harju <git@anttiharju.dev>
2024-12-24 18:10:58 +0200

    5.23

--- part5/e2e-tests/tests/helper.js
+++ part5/e2e-tests/tests/helper.js
@@ -1,3 +1,5 @@
+const { expect } = require('@playwright/test')
+
 const loginWith = async (page, username, password)  => {
   await page.getByTestId('username').fill(username)
   await page.getByTestId('password').fill(password)
@@ -10,10 +12,11 @@ const createBlog = async (page, title, author, url) => {
   await page.getByTestId('blog-author-input').fill(author)
   await page.getByTestId('blog-url-input').fill(url)
   await page.getByRole('button', { name: 'create' }).click()
+  await expect(page.getByText(`${title} by ${author} added`)).toBeVisible()
 }
 
-const getLikes = async (page) => {
-  const likesText = await page.locator('.likes').textContent()
+const getLikes = async (page, n=0) => {
+  const likesText = await page.locator('.likes').nth(n).textContent()
   const N = 1 // format: "likes N"
   return parseInt(likesText.split(' ')[N])
 }
